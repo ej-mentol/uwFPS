c9e298ecb66bc94cc14da5ec5cd6dfa0f5d1d0b3
ej-mentol <mentolusunit2@gmail.com>
2026-09-23 15:32:30 +0300

    bump version to 1.4.0

--- plugins.h
+++ plugins.h
@@ -5,8 +5,13 @@
 #include "HLSDK/common/interface.h"
 #include "HLSDK/common/cvardef.h"
 #include "HLSDK/common/ref_params.h"
+#include "HLSDK/common/entity_state.h"
+#include "HLSDK/common/cl_entity.h"
+#include "HLSDK/pm_shared/pm_shared.h"
 
 #include <string>
+
+#define UWFPS_VERSION "1.4.0"
 
 extern cvar_t* uw_auto;
 extern cvar_t* uw_fps_underwater;
@@ -14,6 +19,7 @@ extern cvar_t* uw_fps_normal;
 extern cvar_t* uw_delay;
 extern cvar_t* uw_mode;
 extern cvar_t* uw_trigger_level;
+extern cvar_t* uw_saved_fps;   // crash-safety: original fps_max persisted to config.cfg
 
 extern cl_enginefunc_t gEngfuncs;
 extern cl_exportfuncs_t gExportfuncs;
@@ -22,11 +28,13 @@ extern metahook_api_t* g_pMetaHookAPI;
 extern void (*g_pfnHUD_Init)(void);
 extern void (*g_pfnHUD_Frame)(double time);
 extern int  (*g_pfnHUD_VidInit)(void);
+extern void (*g_pfnHUD_Reset)(void);
 extern void (*g_pfnV_CalcRefdef)(struct ref_params_s* pparams);
 
 void HUD_Init(void);
 void HUD_Frame(double time);
 int  HUD_VidInit(void);
+void HUD_Reset(void);
 void V_CalcRefdef(struct ref_params_s* pparams);
 void uwFPS_Init(void);
 void uw_toggle_f(void);
